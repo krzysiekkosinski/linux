552b4a3f4ce050cd2eed6456e4b9e7c17992d0fd
root <krzysztofkosinski@interia.pl>
2017-03-21 16:00:42 +0100

    przeniesienie aplikacji

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn' => 'mysql:dbname=u723439948_data;host=localhost',
+        'dsn' => 'mysql:dbname=extranet2;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
